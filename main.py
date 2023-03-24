838f1b4402449937bff977f3d430b0b2cb481e47
Harshit <raoharshit2025@gmail.com>
2023-03-24 19:10:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pickle
 
 st.set_page_config(page_title="University Recommendation System")
 
-
 # Load the model
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
@@ -16,7 +15,7 @@ toefl = st.number_input("TOEFL Score", min_value=0, max_value=120, step=1)
 
 # Make predictions
 if st.button("Submit"):
-    data = {'CGPA': [cgpa], 'GRE Score': [gre], 'TOEFL Score': [toefl]}
+    data = pd.DataFrame({'CGPA': [cgpa], 'GRE Score': [gre], 'TOEFL Score': [toefl]})
     prediction = model.predict([[cgpa, gre, toefl]])
     filtered_data = data[(data['CGPA'] <= cgpa) & (data['GRE Score'] <= gre) & (data['TOEFL Score'] <= toefl)]
     #print(f"Values less than {cgpa and gre and toefl}: \n{filtered_data}")
