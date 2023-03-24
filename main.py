0a12f9e94c20123b5bae2a7d830e8f92bbe660d5
Harshit <raoharshit2025@gmail.com>
2023-03-24 18:05:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import streamlit as st
+import pandas as pd
+import pickle
 
+st.set_page_config(page_title="University Recommendation System")
+
+# Load the model
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-def app():
-    st.title('University Admission Predictor')
-    st.write('Enter your details to get admission prediction')
+# Define the form inputs
+cgpa = st.number_input("CGPA", min_value=0.0, max_value=10.0, step=0.1, format="%.1f")
+gre = st.number_input("GRE Score", min_value=0, max_value=340, step=1)
+toefl = st.number_input("TOEFL Score", min_value=0, max_value=120, step=1)
 
-# Define the input form
-form = st.form(key='input_form')
-cgpa = form.number_input('CGPA', min_value=0.0, max_value=10.0, step=0.01, format="%.2f")
-gre = form.number_input('GRE Score', min_value=0, max_value=340, step=1)
-toefl = form.number_input('TOEFL Score', min_value=0, max_value=120, step=1)
-submit_button = form.form_submit_button('Submit')
-
-# Get the prediction result
-if submit_button:
-    result = model.predict([[cgpa, gre, toefl]])
-    if result == 1:
-        st.write('Congratulations! You are likely to get admission.')
+# Make predictions
+if st.button("Submit"):
+    data = {'CGPA': [cgpa], 'GRE Score': [gre], 'TOEFL Score': [toefl]}
+    df = pd.DataFrame(data)
+    prediction = model.predict(df)
+    if prediction[0] == 1:
+        st.write("You have a good chance of getting admission to the university!")
     else:
-        st.write('Sorry, you are not likely to get admission.')
-
-if name == 'main':
-    app()
+        st.write("Sorry, your chances of getting admission to the university are low.")
