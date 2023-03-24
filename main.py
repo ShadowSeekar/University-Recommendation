fb8936464a1db4a9b5c1449f1b408116286562b2
Harshit <raoharshit2025@gmail.com>
2023-03-24 18:31:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import pickle
