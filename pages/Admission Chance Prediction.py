bf39c2aa33d2b82bbf39f7ee93054a2b27834635
Harshit <raoharshit2025@gmail.com>
2023-03-28 16:54:02 +0530

    Update Admission Chance Prediction.py

--- pages/Admission Chance Prediction.py
+++ pages/Admission Chance Prediction.py
@@ -22,8 +22,8 @@ def app():
     
     # Create the input form for the user
     form = st.form(key='admission_chance_form')
-    gre_score = form.number_input('GRE Score', min_value=0, max_value=340, step=1)
-    toefl_score = form.number_input('TOEFL Score', min_value=0, max_value=120, step=1)
+    gre_score = form.number_input('GRE Score', min_value=250, max_value=340, step=1)
+    toefl_score = form.number_input('TOEFL Score', min_value=80, max_value=120, step=1)
     university_rating = form.slider('University Rating', min_value=1, max_value=5, step=1, value=3)
     sop = form.slider('Statement of Purpose Strength (out of 5)', min_value=0.0, max_value=5.0, step=0.1, value=3.0)
     lor = form.slider('Letter of Recommendation Strength (out of 5)', min_value=0.0, max_value=5.0, step=0.1, value=3.0)
