cef9bbb11fed51f9b68160e0d2d0adb7d386e996
Harshit <raoharshit2025@gmail.com>
2023-04-01 02:09:07 +0530

    Update Info.py

--- pages/Info.py
+++ pages/Info.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
-st.markdown("# How is this different from the previous one? ❄️")
+st.markdown("# Machine Learning algorithms used ❄️")
 st.sidebar.markdown("# Working and Explaination ❄️")
-st.markdown('#### It uses Random Forest machine learning algorithm to predict results.')
-st.markdown('##### Model Accuracy: 0.9933333333333333 or 99.33%')
-st.write("Random forest is a type of machine learning algorithm used for classification problems, where we want to predict a categorical label for each data point based on some input features.")
-st.write("In this code, the input features are the CGPA, GRE Score, and TOEFL Score of students, and the goal is to predict whether each student will get admission to a university or not. The admission decision is based on whether the student's CGPA, GRE Score, and TOEFL Score are all below certain thresholds.")
-st.write("The code uses a dataset containing these features and admission labels, and splits it into a training set and a testing set. It then trains a random forest classifier using the training set, where the classifier is a model that learns to map the input features to the corresponding admission label. The random forest classifier works by constructing a large number of decision trees, each of which makes a prediction based on a subset of the input features.")
-st.write("The final prediction of the random forest is then determined by aggregating the predictions of all the individual decision trees. By training this random forest classifier on the labeled data, the code aims to learn a model that can accurately predict the admission decision for new, unseen data based on their CGPA, GRE Score, and TOEFL Score.")
+st.markdown('#### The recommender uses Random Forest machine learning algorithm to predict results.')
+st.markdown('##### Model Accuracy: 99.33%')
+st.write("Random forest classifier is a type of machine learning algorithm used for classification problems, where we want to predict a categorical label for each data point based on some input features."\n)
+
+st.markdown('#### Admission Predictor uses K-Nearest Neighbor machine learning algorithm to predict results.')
+st.markdown('##### Model Accuracy: 99.87%')
+st.write("K-nearest neighbor (KNN) regression is a machine learning algorithm used for regression tasks. It works by finding the K training examples that are closest to a given test example and using their target values to predict the target value of the test example.")
