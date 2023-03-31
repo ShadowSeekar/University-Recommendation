c1c23e2b4ddbdd27de05c1941cd8267a53a2a24b
Harshit <raoharshit2025@gmail.com>
2023-04-01 02:09:54 +0530

    Update Info.py

--- pages/Info.py
+++ pages/Info.py
@@ -4,7 +4,7 @@ st.markdown("# Machine Learning algorithms used ❄️")
 st.sidebar.markdown("# Working and Explaination ❄️")
 st.markdown('#### The recommender uses Random Forest machine learning algorithm to predict results.')
 st.markdown('##### Model Accuracy: 99.33%')
-st.write("Random forest classifier is a type of machine learning algorithm used for classification problems, where we want to predict a categorical label for each data point based on some input features."\n)
+st.write("Random forest classifier is a type of machine learning algorithm used for classification problems, where we want to predict a categorical label for each data point based on some input features.")
 
 st.markdown('#### Admission Predictor uses K-Nearest Neighbor machine learning algorithm to predict results.')
 st.markdown('##### Model Accuracy: 99.87%')
