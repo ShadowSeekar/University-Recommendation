20bc68009a4294adcbd61c7046fc7b4f27ff30f6
Harshit <raoharshit2025@gmail.com>
2023-04-01 02:10:54 +0530

    Update Info.py

--- pages/Info.py
+++ pages/Info.py
@@ -5,7 +5,7 @@ st.sidebar.markdown("# Working and Explaination ❄️")
 st.markdown('#### The recommender uses Random Forest machine learning algorithm to predict results.')
 st.markdown('##### Model Accuracy: 99.33%')
 st.write("Random forest classifier is a type of machine learning algorithm used for classification problems, where we want to predict a categorical label for each data point based on some input features.")
-
+st.markdown('## ')
 st.markdown('#### Admission Predictor uses K-Nearest Neighbor machine learning algorithm to predict results.')
 st.markdown('##### Model Accuracy: 99.87%')
 st.write("K-nearest neighbor (KNN) regression is a machine learning algorithm used for regression tasks. It works by finding the K training examples that are closest to a given test example and using their target values to predict the target value of the test example.")
