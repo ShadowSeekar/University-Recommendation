6af1bd97a9596f8435ffaf14c32feb879b15990e
Harshit <raoharshit2025@gmail.com>
2025-12-20 22:18:12 +0530

    Refactor Admission Predictor

--- pages/Admission Predictor.py
+++ pages/Admission Predictor.py
@@ -5,23 +5,18 @@ import streamlit as st
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.preprocessing import StandardScaler
 
-# Load the trained KNN model from disk
 with open('kNNr_model2.pkl', 'rb') as file:
     knn = pickle.load(file)
 
-# Load the dataset used for training the model
 df_train = pd.read_csv('student_data.csv')
 df_train = df_train.drop(['SOP', 'LOR', 'Research'], axis=1)
 
-# Separate the features and target variable
 X_train = df_train[['GRE Score', 'TOEFL Score', 'CGPA']]
 y_train = df_train['Chance of Admit']
 
-# Fit a StandardScaler instance to the training data and use it to scale the user input
 scaler = StandardScaler()
 scaler.fit(X_train)
 
-# Define a function to get the top 5 institutions with predicted chances of admission that are lower than the user input
 def get_top_institutions(user_input):
     user_input = np.array(user_input).reshape(1, -1)
     user_input = scaler.transform(user_input)
@@ -31,7 +26,7 @@ def get_top_institutions(user_input):
     top_institutions = df_grouped[df_grouped['Predicted Chance of Admit (%)'] < predicted_chance_of_admit].head(5)
     return top_institutions[['Predicted Chance of Admit (%)']]
 
-# Create the web app using Streamlit
+
 st.title('Admission Predictor')
 
 st.write('Enter the values of the following features:')
@@ -47,7 +42,6 @@ cgpa = st.slider('CGPA', 6.0, 10.0, 8.0)
 #    research = 0
 
 
-# Get the top 5 institutions with predicted chances of admission that are lower than the user input
 user_input = [gre_score, toefl_score, cgpa]
 top_institutions = get_top_institutions(user_input)
 
@@ -56,4 +50,5 @@ if top_institutions.empty:
 else:
     st.write('Institutions with predicted chances of admission:')
     st.table(top_institutions)
+
 
